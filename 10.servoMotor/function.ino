d949364b0d21f4bea8014d1812a7c85b9d1a11c9
ZhaoHoangLi <126206745+ZhaoHoangLi@users.noreply.github.com>
2023-09-12 09:44:26 +1000

    complete 10

--- 10.servoMotor/function.ino
+++ 10.servoMotor/function.ino
@@ -1,16 +1,23 @@
-void openGate () {
+bool openGate () {
+
  for(int i = 0; i <= 255; i++) {
-  myServo.write(i);
-  delay(75); 
- }
+    myServo.write(255-i);
+     delay(15); 
+      isGateOpen = true;
+    }
 }
 
 void closeGate () {
-myServo.write(0);
-delay(1000);
+
+    for(int i = 0; i <= 255; i++) {
+     myServo.write(255-i);
+     delay(15);
+     isGateOpen = false;
+    }
 }
 
-void readUS () {
+unsigned long readUS () {
   unsigned long rangeInCM = myUSsensor.MeasureInCentimeters();
   Serial.println(rangeInCM);
+  return rangeInCM;
 }
